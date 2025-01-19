094f8605ea305ebad729bf7bc2cdd7ace01bcf22
JonathanBokeunChang <jonathanbokeunchang@gmail.com>
2025-01-19 18:07:35 -0500

    Initial Commit

---
+++ StoryApp/StoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryAppApp.swift
+//  StoryApp
+//
+//  Created by Jonathan Chang on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct StoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoryAppTests/StoryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryAppTests.swift
+//  StoryAppTests
+//
+//  Created by Jonathan Chang on 1/19/25.
+//
+
+import Testing
+@testable import StoryApp
+
+struct StoryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
